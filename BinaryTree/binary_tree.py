84349095c4a9d24522f4ab0a08597e319688fc28
Victor Hugo Dias de Oliveira <victorhugodias2001@gmail.com>
2024-06-14 19:54:28 -0300

    post order commit

--- BinaryTree/binary_tree.py
+++ BinaryTree/binary_tree.py
@@ -9,17 +9,40 @@ class BinaryTree:
             self.root = None
 
     # Symmetric Order in Tree Traversal
-    def simetric_traversal(self, node=None):
+    def in_order(self, node=None):
         if node is None:
             node = self.root
         if node.left:
             print('(', end='')
-            self.simetric_traversal(node.left)
+            self.in_order(node.left)
         print(node, end='')
         if node.right:
-            self.simetric_traversal(node.right)
+            self.in_order(node.right)
             print(')', end='')
 
+    def post_order_traversal(self, node=None):
+        if node is None:
+            node = self.root
+        if node.left:
+            self.post_order_traversal(node.left)
+        if node.right:
+            self.post_order_traversal(node.right)
+        print(node, end="")
+
+    def height(self, node=None):
+        if node is None:
+            node = self.root
+        hleft = 0
+        hright = 0
+        if node.left:
+            hleft = self.height(node.left)
+        if node.right:
+            hright = self.height(node.right)
+        if hright > hleft:
+            return hright+1
+        return hleft+1
+
+# in order test
 tree = BinaryTree()
 n1 = Node('a')
 n2 = Node('+')
@@ -39,6 +62,38 @@ n3.left = n4
 n3.right = n5
 n2.left = n1
 n2.right = n3
-tree.root = n2
+tree.root = n2 # change root to see another height
 
-tree.simetric_traversal()
+tree.in_order()
+
+print("")
+print(f"Height: {tree.height()}")
+
+
+# post order test
+n1 = Node('f')
+n2 = Node('r')
+n3 = Node('i')
+n4 = Node('e')
+n5 = Node('n')
+n6 = Node('d')
+n7 = Node('s')
+n8 = Node('h')
+n9 = Node('i')
+n0 = Node('p')
+
+n0.left = n6
+n0.right = n9
+n6.left = n1
+n6.right = n5
+n5.left = n2
+n5.right = n4
+n4.right = n3
+n9.left = n8
+n8.right = n7
+tree.root = n0 # change root to see another height
+
+tree.post_order_traversal()
+
+print("")
+print(f"Height: {tree.height()}")
